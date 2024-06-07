c61e6da5f9ca89bf33c6798938defc19e89b6c7b
thienc45 <doanducthien123@gmail>
2024-06-07 14:17:22 +0700

    commit first

---
+++ src/test/java/com/example/send/devopsautomation/DevopsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.send.devopsautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
